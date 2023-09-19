39e628fa5140a3771048987a99a8115f53d2f364
thrallbata <vicortobdel@gmail.com>
2023-09-19 11:16:19 +0700

    update model

--- referral_app/models.py
+++ referral_app/models.py
@@ -18,12 +18,11 @@ class ProfileManager(BaseUserManager):
 
 class Profile(models.Model):
     phone_regex = RegexValidator(
-        regex=r'^(\+\d{1,3})?,?\s?\d{8,13}$',
+        regex=r'^((\+7|7|8)+([0-9]){10})$',
         message="Phone number must be entered in the format: '+79*********'."
     )
     phone = models.CharField(validators=[phone_regex], max_length=17, verbose_name='Номер телефона')
     invite_code = models.CharField(max_length=6, null=False, verbose_name='Код приглашения')
-    code_usage = models.BooleanField(default=False)
     inviter = models.CharField(max_length=6, null=True, verbose_name='Код пригласившего')
 
     object = ProfileManager()
